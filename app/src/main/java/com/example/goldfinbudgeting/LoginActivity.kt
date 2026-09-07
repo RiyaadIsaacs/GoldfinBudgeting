9dd583671e534d173ef37d750e7cc0de9b15fc70
Lazjessicah <166005469+LJ-vdZ@users.noreply.github.com>
2026-09-07 15:34:47 +0200

    Added functionality to input fields and button, and removed project template's self generated xml and kt files to avoid confusion
    - created placeholder home screen to test sign in (working)
    - email to sign in = 1234@gmail.com
    - password to sign in = 1234

--- app/src/main/java/com/example/goldfinbudgeting/LoginActivity.kt
+++ app/src/main/java/com/example/goldfinbudgeting/LoginActivity.kt
@@ -1,20 +1,54 @@
 package com.example.goldfinbudgeting
 
+
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
+import android.content.Intent
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val emailEditText = findViewById<EditText>(R.id.emailEditText)
+
+        val passwordEditText = findViewById<EditText>(R.id.passwordEditText)
+
+        val signInButton = findViewById<Button>(R.id.signInButton)
+
+        //listen for when user clicks button to sign in
+        signInButton.setOnClickListener {
+
+            val email = emailEditText.text.toString().trim()
+
+            val password = passwordEditText.text.toString().trim()
+
+            //check if field were filled before signing in and going to home screen
+            if (email.isEmpty() || password.isEmpty())
+            {
+                //give message to fill in fields
+                Toast.makeText(this, "Please fill in both fields", Toast.LENGTH_SHORT).show()
+            }
+            //this is the set email and password to signin
+            else if (email == "1234@gmail.com" && password == "1234")
+            {
+                Toast.makeText(this, "Signing in...", Toast.LENGTH_SHORT).show()
+
+                //open and run home screen with correct email and password
+                val intent = Intent(this, HomeActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+            else //when email or password is incorrect send message. dont go to next screen
+            {
+                Toast.makeText(this, "Incorrect email or password", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
