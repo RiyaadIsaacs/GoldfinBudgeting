ef089ac21358270abe16ace19b05200054d2819c
Lazjessicah <166005469+LJ-vdZ@users.noreply.github.com>
2026-09-10 18:05:42 +0200

    added side drawer for hamburger icon and functionality for logout button

--- app/src/main/java/com/example/goldfinbudgeting/HomeActivity.kt
+++ app/src/main/java/com/example/goldfinbudgeting/HomeActivity.kt
@@ -1,20 +1,51 @@
 package com.example.goldfinbudgeting
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
 
 class HomeActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_home)
+
+        //keep content from hiding behind status bar
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        //find the views we need by their id
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawerLayout)
+        val menuIcon = findViewById<TextView>(R.id.menuIcon)
+        val closeDrawerButton = findViewById<TextView>(R.id.closeDrawerButton)
+        val logOutButton = findViewById<TextView>(R.id.logOutButton)
+
+        //open side menu
+        menuIcon.setOnClickListener {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+
+        //close side menu
+        closeDrawerButton.setOnClickListener {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        }
+
+        //go back to log in screen
+        logOutButton.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 }
